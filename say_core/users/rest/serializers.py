8fde733c56635e46636606c116f04a4ff32f6993
Amir Khalife <eng.amir.bu@gmail.com>
2024-03-23 13:49:41 +0330

    rm boilerplate views

--- say_core/users/rest/serializers.py
+++ say_core/users/rest/serializers.py
@@ -1,14 +1,3 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
 
 UserModel = get_user_model()
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserModel
-        fields = ["username", "name", "url"]
-
-        extra_kwargs = {
-            "url": {"view_name": "rest:user-detail", "lookup_field": "username"},
-        }
